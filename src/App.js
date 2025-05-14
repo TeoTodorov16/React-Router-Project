75e78f9566b1d57540d0436682ac98b77b3676da
Teodor <teodorttodorov@icloud.com>
2025-05-14 12:31:11 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
-const notesa = createBrowserRouter([
+const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
-  {},
   {},
   {},
 ]);
 
 function App() {
-  return <RouterProvider router={notesa} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
