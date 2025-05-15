18e063191ed8884aadb61533a82f7c8787092371
Teodor <teodorttodorov@icloud.com>
2025-05-15 16:11:51 +0300

    Test1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  {},
-  {},
-]);
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
 
 function App() {
   return <RouterProvider router={router} />;
